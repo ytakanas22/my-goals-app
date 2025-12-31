6815c15cdc10c7fe069ddd7d7b301d50dd8cf741
ytakanas22 <ytakanas22@gmail.com>
2025-12-31 07:12:53 +0000

    fix login dialog

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { AuthService } from './services/auth.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule, MatFormFieldModule, MatInputModule],
+  imports: [RouterOutlet, MatCardModule, MatFormFieldModule, MatInputModule, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
